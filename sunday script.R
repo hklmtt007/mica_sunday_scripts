58bb7eb74c449449deeff61790144ff05d96f046
wulawula <hemin090122@gmail.com>
2015-04-19 14:07:58 -0600

    continue to update the r script

--- sunday script.R
+++ sunday script.R
@@ -3,4 +3,16 @@
 # April 19, 2015
 
 # read in data as tab-delimited
-gap.in <- read.table("output/combined_gapMinder.tsv",sep = "\t")
+gap.in <- read.table("output/combined_gapMinder.tsv",sep = "\t",header = TRUE)
+
+  
+
+
+for (row.number in 1:10){for(col.number in 1:5){print(gap.in[row.number,col.number]))}}
+
+gap.in[1:10,1:5]
+
+add.me <- function(x,y){(x+y)*8}
+
+add.me(3,4)
+
