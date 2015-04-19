2430bbaa3dcb2beb51902da990b01f700688ba40
wulawula <hemin090122@gmail.com>
2015-04-19 14:07:31 -0600

    remove single quote character from gapminder data

--- sunday script.R
+++ sunday script.R
@@ -1,3 +1,6 @@
 # this is the script to explore the gapminder dataset and using R
 # min he
 # April 19, 2015
+
+# read in data as tab-delimited
+gap.in <- read.table("output/combined_gapMinder.tsv",sep = "\t")
